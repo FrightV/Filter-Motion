8d7c24cf2495a56cd9c6fd92e9dcc0093ebeb94d
FrightV <victorderumbutis@gmail.com>
2026-03-02 16:46:44 -0300

    Initial commit - projeto Spring Boot base

---
+++ src/main/java/com/filtermotion/FilterMotionApplication.java
@@ -0,0 +1,13 @@
+package com.filtermotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterMotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilterMotionApplication.class, args);
+	}
+
+}
